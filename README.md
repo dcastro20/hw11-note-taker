650fa2fc6cdb23334edef436fa0b4ec3a37b426a
Doris Castro <dorisgeo12@gmail.com>
2020-05-25 22:36:20 -0400

    updating api file and readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# hw11-note-taker
+# Title
 Note Taker
+hw11-note-taker
+
+
+## Description
+Note Taker
+ This project allows the user to input notes needed.
+
+## Table of Contents
+
+  *1. Title *2. Description *3. TOC *4. Installation *5. Usage
+
+## Installation
+
+fill out the note tile and the note.  on the left panel you can delete the note.
+
+## Usage
+
+  this project would be useful for someone taking notes
+
---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+var tableData = require("../db/db.json");
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        console.log(tableData);
+        res.json(tableData);
+    });
+
+    app.post("/api/notes", function (req, res) {
+        req.body.id = tableData.length + 1;
+        tableData.push(req.body)
+        res.json(true);
+    });
+    app.delete("/api/notes/:id", function (req, res) {
+        console.log(req.params.id);
+        const newNotesList = tableData.filter(note => note.id !== req.params.id);
+        console.log(newNotesList);
+        tableData = newNotesList;
+        res.json(tableData);
+    });
+}
